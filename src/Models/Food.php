a549df82b0dcde74c30f229cb68f9b4d9908a77f
yh1126 <cake_sk_yh@icloud.com>
2019-09-25 17:25:00 +0900

    大きなおむすびマヨネーズの税抜き価格が取得できる

--- src/Models/Food.php
+++ src/Models/Food.php
@@ -7,10 +7,20 @@ use App\Models\Item;
 class Food extends Item
 {
     private $tax_exclude_price;
+    private $name;
+    const LIST = [
+        '手巻直火焼き紅しゃけ' => [
+            'tax_exclude_price' => 139
+        ],
+        '大きなおむすびマヨネーズ' => [
+            'tax_exclude_price' => 186
+        ]
+    ];
 
-    public function __construct()
+    public function __construct($name)
     {
-        $this->tax_exclude_price = 139;
+        $this->name = $name;
+        $this->tax_exclude_price = self::LIST[$this->name]['tax_exclude_price'];
     }
 
     public function isReducedTaxRate(): bool
--- tests/Models/FoodTest.php
+++ tests/Models/FoodTest.php
@@ -10,12 +10,18 @@ class FoodTest extends TestCase
     /** @test */
     public function 軽減税率の対象である()
     {
-        $this->assertTrue((new Food())->isReducedTaxRate());
+        $this->assertTrue((new Food($name = '手巻直火焼き紅しゃけ'))->isReducedTaxRate());
     }
 
     /** @test */
     public function 手巻直火焼き紅しゃけの税抜き価格が取得できる()
     {
-        $this->assertEquals(139, (new Food())->tax_exclude_price);
+        $this->assertEquals(139, (new Food($name = '手巻直火焼き紅しゃけ'))->tax_exclude_price);
+    }
+
+    /** @test */
+    public function 大きなおむすびマヨネーズの税抜き価格が取得できる()
+    {
+        $this->assertEquals(186, (new Food($name = '大きなおむすびマヨネーズ'))->tax_exclude_price);
     }
 }
