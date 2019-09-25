60a8871fe0e233354f825e7570dcf457d3649d43
h_yamashita <h_yamashita@corp.allabout.co.jp>
2019-09-25 16:24:07 +0900

    キリン生茶555mlペットボトルを渡すとbeverageクラスが返ってくる

--- src/Factories/ItemFactory.php
+++ src/Factories/ItemFactory.php
@@ -2,12 +2,16 @@
 
 namespace App\Factories;
 
+use App\Models\Beverage;
 use App\Models\Food;
 
 class ItemFactory
 {
     public static function create($name)
     {
+        if ($name === 'キリン生茶555mlペットボトル') {
+            return new Beverage();
+        }
         return new Food();
     }
 }
--- tests/Factories/ItemFactoryTest.php
+++ tests/Factories/ItemFactoryTest.php
@@ -4,6 +4,7 @@ namespace Tests\Factories;
 
 use App\Factories\ItemFactory;
 use App\Models\Food;
+use App\Models\Beverage;
 use Tests\TestCase;
 
 class ItemFactoryTest extends TestCase
@@ -12,5 +13,11 @@ class ItemFactoryTest extends TestCase
     public function 手巻直火焼き紅しゃけを渡すとfoodクラスが返ってくる()
     {
         $this->assertInstanceOf(Food::class, ItemFactory::create($name = '手巻直火焼き紅しゃけ'));
+    }
+
+    /** @test */
+    public function キリン生茶555mlペットボトルを渡すとbeverageクラスが返ってくる()
+    {
+        $this->assertInstanceOf(Beverage::class, ItemFactory::create($name = 'キリン生茶555mlペットボトル'));
     }
 }
