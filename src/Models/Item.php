62ad61846539074b71597ddc092ae4b7e1067c2f
h_yamashita <h_yamashita@corp.allabout.co.jp>
2019-09-25 16:08:07 +0900

    キリンチューハイ氷結グレープフルーツ350ml缶は軽減税率対象でない

--- src/Models/Item.php
+++ src/Models/Item.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 class Item
 {
+    private $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
     public function isReducedTaxRate()
     {
+        if ($this->name === 'キリンチューハイ氷結グレープフルーツ350ml缶') {
+            return false;
+        }
         return true;
     }
 }
--- tests/Models/ItemTest.php
+++ tests/Models/ItemTest.php
@@ -10,18 +10,24 @@ class ItemTest extends TestCase
     /** @test */
     public function 手巻直火焼き紅しゃけは軽減税率対象である()
     {
-        $this->assertTrue((new Item())->isReducedTaxRate());
+        $this->assertTrue((new Item($name = '手巻直火焼き紅しゃけ'))->isReducedTaxRate());
     }
 
     /** @test */
     public function 大きなおむすびマヨネーズは軽減税率対象である()
     {
-        $this->assertTrue((new Item())->isReducedTaxRate());
+        $this->assertTrue((new Item($name = '大きなおむすびマヨネーズ'))->isReducedTaxRate());
     }
 
     /** @test */
     public function キリン生茶555mlペットボトルは軽減税率対象である()
     {
-        $this->assertTrue((new Item())->isReducedTaxRate());
+        $this->assertTrue((new Item($name = 'キリン生茶555mlペットボトル'))->isReducedTaxRate());
+    }
+
+    /** @test */
+    public function キリンチューハイ氷結グレープフルーツ350ml缶は軽減税率対象でない()
+    {
+        $this->assertFalse((new Item($item_name = 'キリンチューハイ氷結グレープフルーツ350ml缶'))->isReducedTaxRate());
     }
 }
