96b35a7c0bfe8f5dcb2b810a78f312a26d698712
yh1126 <cake_sk_yh@icloud.com>
2019-09-25 17:46:34 +0900

    キリン生茶555mlペットボトル

--- src/Models/Beverage.php
+++ src/Models/Beverage.php
@@ -6,8 +6,26 @@ use App\Models\Item;
 
 class Beverage extends Item
 {
+    private $name;
+
+    const LIST = [
+        'キリン生茶555mlペットボトル' => [
+            'tax_exclude_price' => 140
+        ]
+    ];
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
     public function isReducedTaxRate(): bool
     {
         return true;
+    }
+
+    public function excludeTaxPrice(): int
+    {
+        return self::LIST[$this->name]['tax_exclude_price'];
     }
 }
--- tests/Models/BeverageTest.php
+++ tests/Models/BeverageTest.php
@@ -10,6 +10,12 @@ class BeverageTest extends TestCase
     /** @test */
     public function 軽減税率対象である()
     {
-        $this->assertTrue((new Beverage())->isReducedTaxRate());
+        $this->assertTrue((new Beverage($name = 'キリン生茶555mlペットボトル'))->isReducedTaxRate());
+    }
+
+    /** @test */
+    public function キリン生茶555mlペットボトルの税抜価格が取得できる()
+    {
+        $this->assertEquals(140, (new Beverage($name = 'キリン生茶555mlペットボトル'))->excludeTaxPrice());
     }
 }
