1604ce06a822291e2032778d9c3a1ce08780bdc5
yh1126 <cake_sk_yh@icloud.com>
2019-09-25 17:15:27 +0900

    手巻直火焼き紅しゃけの税抜き価格が取得できる

--- src/Models/Food.php
+++ src/Models/Food.php
@@ -6,8 +6,20 @@ use App\Models\Item;
 
 class Food extends Item
 {
+    private $tax_exclude_price;
+
+    public function __construct()
+    {
+        $this->tax_exclude_price = 139;
+    }
+
     public function isReducedTaxRate(): bool
     {
         return true;
+    }
+
+    public function __get($name)
+    {
+        return $this->$name;
     }
 }
--- tests/Models/FoodTest.php
+++ tests/Models/FoodTest.php
@@ -12,4 +12,10 @@ class FoodTest extends TestCase
     {
         $this->assertTrue((new Food())->isReducedTaxRate());
     }
+
+    /** @test */
+    public function 手巻直火焼き紅しゃけの税抜き価格が取得できる()
+    {
+        $this->assertEquals(139, (new Food())->tax_exclude_price);
+    }
 }
